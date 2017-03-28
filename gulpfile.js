32a54bffb5b6a9acf2e474bb6c0da9dfbe910437
Marcelo Barth <mbarth@xerafloptech.com>
2017-03-27 22:08:38 -0700

    Converted css to sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,37 @@
+var rimraf = require('gulp-rimraf');
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
-var IMAGES = "src/images/*.*";
+var rename = require('gulp-rename');
 var JS_SRC = ["src/js/config.js", "src/js/emojiPlugin.js", "src/js/emojiInsertDirective.js", "src/js/jquery.emojiarea.js", "src/js/nanoscroller.js", "src/js/util.js"];
-var CSS_SRC = "src/css/*.css";
 
-/**
- *  Manage Core Files
- **/
-gulp.task('copy-images', function () {
-    return gulp.src(IMAGES)
+gulp.task('compile-css', function() {
+    gulp.src('src/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css'));
+});
+gulp.task('core-css', ['compile-css', 'empty-dist'], function () {
+    return gulp.src('src/css/*.css')
+        .pipe(uglifycss())
+        .pipe(rename('emoji.min.css'))
+        .pipe(gulp.dest('dist/css'))
+});
+gulp.task('copy-images', ['empty-dist'], function () {
+    return gulp.src('src/images/*.*')
         .pipe(gulp.dest('dist/images'));
 });
-gulp.task('core-js', function () {
+gulp.task('core-js', ['empty-dist'], function () {
     return gulp.src(JS_SRC)
         .pipe(plumber())
         .pipe(concat('emoji.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 });
-gulp.task('core-css', function () {
-    return gulp.src(CSS_SRC)
-        .pipe(plumber())
-        .pipe(concat('emoji.min.css'))
-        .pipe(uglifycss())
-        .pipe(gulp.dest('dist/css'));
+gulp.task('empty-dist', function() {
+    return gulp.src('dist/', { read: false })
+        .pipe(rimraf());
 });
 gulp.task('default', ['copy-images', 'core-js', 'core-css']);
